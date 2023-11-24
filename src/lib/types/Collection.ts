91815704879e1e839f6144ee2b985fe7da91ea07
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2023-11-24 22:07:04 +0100

    fix: collections expand cards in connection

--- src/lib/types/Collection.ts
+++ src/lib/types/Collection.ts
@@ -1,9 +1,35 @@
 import type {Card} from "$lib/types/Card";
 
+/**
+ * A collection is a group of cards.
+ * Use `RawCollection` when getting data from PocketBase and
+ * convert it to `Collection` using `intoCollection()`.
+ */
 export interface Collection {
     card_ids: Card[];
     id: string;
     name: string;
     created: string;
     updated: string;
+}
+
+export interface RawCollection {
+    expand: {
+        card_ids: Card[];
+    };
+    id: string;
+    name: string;
+    created: string;
+    updated: string;
+    card_ids: string[];
+}
+
+export function intoCollection(data: RawCollection): Collection {
+    return {
+        card_ids: data.expand.card_ids,
+        id: data.id,
+        name: data.name,
+        created: data.created,
+        updated: data.updated
+    }
 }
