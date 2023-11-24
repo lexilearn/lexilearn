60d2f4d443699cd313bb7f7fd66d72a13d66edb3
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2023-11-24 20:55:24 +0100

    feat: scripts to create sample data

---
+++ scripts/create_data.js
@@ -0,0 +1,16 @@
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase("https://pocketbase.oskar.global");
+await pb.admins.authWithPassword(process.env["PB_USER"], process.env["PB_PASSWORD"]);
+
+const data = await Bun.file("data.csv").text();
+const lines = data.split("\n");
+
+for (let line of lines) {
+    const [question, answer] = line.split(",");
+
+    await pb.collection('cards').create({
+        question,
+        answer,
+    });
+}
