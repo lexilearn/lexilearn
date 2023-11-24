8ed64967839eb9559d208a6cf61837e51664f8d0
DieProgrammIDE <programmide@dieprogrammi.de>
2023-11-24 22:39:11 +0000

    feat: save progess on frontend work
    Co-authored-by: Oskar Manhart <oskardotglobal@users.noreply.github.com>
    Co-authored-by: Adam Brangenberg <adambrangenberg@proton.me>
    Co-authored-by: fr-edy <fr-edy@users.noreply.github.com>

---
+++ src/lib/components/library/card-set.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { Collection } from "$lib/types/Collection";
+    export let collection: Collection;
+</script>
+
+<div class="card-set">
+    <h2>{collection.name}</h2>
+</div>
