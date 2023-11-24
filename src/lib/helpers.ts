ade4cc9a02d13fe9d3b8d682bf05da0e53b3c330
adambrangenberg <adambrangenberg@proton.me>
2023-11-24 20:54:51 +0100

    feat: adding helpers

---
+++ src/lib/helpers.ts
@@ -0,0 +1,40 @@
+import PocketBase from 'pocketbase'
+// @ts-ignore deez nuts
+import * as process from "node:process";
+
+
+const DB_URL = "https://pocketbase.oskar.global"
+export async function connectDbAsAdmin(): Promise<PocketBase> {
+  const db = new PocketBase(DB_URL)
+  await db.admins.authWithPassword(process.env["PB_USER"], process.env["PB_PASSWORD"])
+
+  if (db.authStore.isValid) {
+    console.log("Connected to PocketBase")
+    return db
+  } else {
+    throw new Error("Could not connect to PocketBase")
+  }
+}
+
+/**
+ * Authenticate with PocketBase as a user.
+ * Important: Please redirect to https://yourdomain.com/api/oauth2-redirect
+ * @param provider
+ */
+export async function connectDbAsUser(provider: string): Promise<PocketBase> {
+  const db = new PocketBase(DB_URL)
+  await db.collection("users").authWithOAuth2({
+    provider: provider,
+  })
+  if (db.authStore.isValid) {
+    console.log("Connected to PocketBase")
+    return db
+  } else {
+    throw new Error("Could not connect to PocketBase")
+  }
+}
+
+
+
+
+
