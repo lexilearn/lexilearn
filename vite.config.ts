37311659df126b072c13682edb2ea14353e667f2
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2023-11-24 20:11:54 +0100

    feat: add dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import UnoCSS from 'unocss/vite';
+import extractorSvelte from '@unocss/extractor-svelte';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+    UnoCSS({
+      extractors: [
+        extractorSvelte(),
+      ],
+      /* more options */
+    }),
+    sveltekit()
+  ]
 });
