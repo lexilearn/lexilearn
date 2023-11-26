0b86590cbecc6aa9f7196515af27ec7484b7576a
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2023-11-26 13:25:28 +0100

    feat: replace bun with node+pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import extractorSvelte from '@unocss/extractor-svelte';
 
 export default defineConfig({
 	plugins: [
